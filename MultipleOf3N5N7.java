282781cc679321ba9cdf8c9b35ecd5dd105897f2
premi007 <87196212+premi007@users.noreply.github.com>
2022-01-26 14:51:13 +0000

    Create MultipleOf3N5N7.java

---
+++ MultipleOf3N5N7.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class MultipleOf3N5N7
+{
+	public static void main(String[] args)
+	{
+	    Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+	    if(n % 3 == 0 && n % 5 == 0 && n % 7 == 0)
+	    {
+	        System.out.println("Yes");
+	    }
+	    else
+	    {
+	        System.out.println("No");
+	    }
+	}
+}
